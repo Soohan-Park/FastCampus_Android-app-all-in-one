95983a3bfda15b8ee19bcdf9f3a93ae43a5842a1
Soohan Park <725psh@gmail.com>
2021-03-06 16:25:03 +0900

    UI :: 1. How to draw UI

---
+++ app/src/main/java/com/example/fastcampus_kotlin/android_ui/01_How-to-draw-ui.kt
@@ -0,0 +1,7 @@
+package com.example.fastcampus_kotlin.android_ui
+
+// 안드로이드에서 화면을 그리는 방법
+// - xml을 이용한다.
+// - xml이란?
+//    - DSL Language (Domain Specific Language)
+//    - 안드로이드 UI를 그리기 위해 특화된 언어이다.
